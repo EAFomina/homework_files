d0f8be1e9a48d3aa6964285d136d0a8338d2dbd2
Ekaterina Fomina <eafomina2022@mail.ru>
2024-02-08 20:20:45 +0300

    Task 2 completed

--- hw_files_cook_book.py
+++ hw_files_cook_book.py
@@ -9,3 +9,22 @@ def cook_book():
                 dish_list.append({'ingredient_name': ingredient_name, 'quantity': quantity, 'measure': measure})
             cook_book[dish_name] = dish_list
     return cook_book
+
+def get_shop_list_by_dishes(dishes, person_count):
+    cook_book_for_list = cook_book()
+    shop_list = {}
+    for dish in dishes:
+        ingredients = cook_book_for_list.get(dish)
+        for ingredient in ingredients:
+            ingredient_name = ingredient.get('ingredient_name')
+            measure = ingredient.get('measure')
+            quantity = ingredient.get('quantity') * person_count
+            if ingredient_name in shop_list:
+                first_quantity = shop_list.get(ingredient_name).get('quantity')
+                shop_list[ingredient_name] = {'measure': measure, 'quantity': first_quantity + quantity}
+            else:
+                shop_list[ingredient_name] = {'measure': measure, 'quantity': quantity}
+    print(shop_list)
+
+get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
+get_shop_list_by_dishes(['Запеченный картофель', 'Омлет', 'Пюре'], 2)
